947ea27792d16a533e84be24c42ff1098d0dcf24
unknown <rnair20@illinois.edu>
2018-05-05 20:38:11 -0500

    test this

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 # views.py
 
 from flask import render_template,request
-from runquery import search,searchsig
+from ranking import *
 import sys
 
 from app import app
@@ -11,15 +11,10 @@ from app import app
 def index():
     return render_template('index.html')
 
-@app.route("/forward/", methods=['POST'])
-def move_forward():
+@app.route("/results/", methods=['POST'])
+def display_results():
     #Moving forward code
     query2 = request.form['query'].replace(" ", "+")
     topic = request.form['topic']
-    val2=searchsig(query2,topic)
-    for k in val2.keys():
-         query2 = k
-         break
-
-    return render_template('results.html', query=query2);
-#
+    results=getScores(query2,topic)
+    return render_template('results.html', query=query2,results=results)
